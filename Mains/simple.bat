b904cb088f2d842d0b46f7ebb8609f13ad940973
TP <35072994+Tony-Pajamas@users.noreply.github.com>
2022-02-06 21:41:11 -0500

    Rename simple.bat to Mains/simple.bat

---
+++ Mains/simple.bat
@@ -0,0 +1,4 @@
+
+@echo off
+
+python2 C:\Users\TP\Documents\Works\Mains\SimpleHTTPServerWithUpload.py %1
--- simple.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-
-python2 C:\Users\TP\Documents\Works\Mains\SimpleHTTPServerWithUpload.py %1
