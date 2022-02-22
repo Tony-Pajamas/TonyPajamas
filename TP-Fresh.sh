eb51713a210bf4605ece40c90fb1d91507d4683b
TP <35072994+Tony-Pajamas@users.noreply.github.com>
2022-02-22 15:20:52 -0500

    Update TP-Fresh.sh

--- TP-Fresh.sh
+++ TP-Fresh.sh
@@ -136,17 +136,6 @@ function TP {
 	echo ""
 }
 
-function banner {
-
-	#Update Clean  Banner
-	
-	#cp /etc/motd /etc/motd.old
-	#echo "" > /etc/motd
-	#echo "      " >> /etc/motd
-	#echo "Testing Machine" >> /etc/motd
-	#echo "" >> /etc/motd
-	#echo "" >> /etc/motd
-}
 
 ########## Main ##########
 
